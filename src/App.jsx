8f4cec5eb1822590d4f925154167b4cb3e81f18b
artyomdev250 <artyom.dev250@gmail.com>
2025-11-18 00:13:05 +0500

    grid banners

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div className="bg-gray-100 h-screen">
       <Navbar />
+      <Hero />
     </div>
   )
 }
