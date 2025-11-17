4eb4d047646ec5795cb18d3fd84526fca058396b
artyomdev250 <artyom.dev250@gmail.com>
2025-11-17 22:09:33 +0500

    navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
 
+import Navbar from './components/Navbar'
+
 function App() {
   return (
-    <div className="App">
-      <div className='bg-gray-900 min-h-screen p-[25px] lg:flex lg:items-center'>
-        <div className='card'>
-          <b className='text-[22px] leading-[28px] text-primary'>Tailwind CSS Heading</b>
-          <p className='my-[10px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum tempora harum quisquam quo, numquam obcaecati soluta adipisci laudantium laboriosam voluptatem impedit.</p>
-          <button className='btn'>Click Here To Start</button>
-        </div>
-      </div>
+    <div className="bg-gray-100 h-screen">
+      <Navbar />
     </div>
   )
 }
